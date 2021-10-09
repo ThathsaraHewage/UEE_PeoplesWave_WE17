9bfe5a04300fc16470495f7727ca9dcb3e61fa42
ThathsaraHewage <thathsara.hewage99@gmail.com>
2021-10-09 17:59:17 +0530

    changed the view of main menu after login

---
+++ app/src/main/java/com/example/peopleswave/PaymentServices.java
@@ -0,0 +1,14 @@
+package com.example.peopleswave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_services);
+    }
+}
